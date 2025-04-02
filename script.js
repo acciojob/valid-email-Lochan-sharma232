9e205398e7faa99a6cbc2bd5a03110e52fa490d5
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-02 14:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+
 function validEmail(str) {
 	let string=^[A-Za-z_.0-9]{6,30}@[A-Za-z]{4,12}.[A-Za-z]{2,8}$/;
   return string.test(str);
 	//your JS code here.
 }
-
-// Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
