22eb3ed93d69f2361c0357e6024cae0d1bd290fa
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-02 18:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
+// function validEmail(str) {
+// 	let string=/^[A-Za-z_.0-9]{6,30}@[A-Za-z]{4,12}.[A-Za-z]{2,8}$/;
+//   return string.test(str);
+// 	//your JS code here
+// 	}
 function validEmail(str) {
-	let string=^[A-Za-z_.0-9]{6,30}@[A-Za-z]{4,12}.[A-Za-z]{2,8}$/;
-  return string.test(str);
-	//your JS code here.
+    let string = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return string.test(str);
 }
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
